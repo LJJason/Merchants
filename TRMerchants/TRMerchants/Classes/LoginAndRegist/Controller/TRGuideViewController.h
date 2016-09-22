3fa6fa0a15c34db9201ef05e5656a8689a0ecd6f
CoderGFWu <wgf@wgfdeMacBook-Air.local>
2016-09-22 16:00:03 +0800

    商家版临居
    初始化项目, 配置应用程序图标

---
+++ TRMerchants/TRMerchants/Classes/LoginAndRegist/Controller/TRGuideViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRGuideViewController.h
+//  TRMerchants
+//
+//  Created by wgf on 16/9/21.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRGuideViewController : UIViewController
+
+@end
---
+++ TRMerchants/TRMerchants/Classes/Main/Controller/TRMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  TRMainViewController.h
+//  TRMerchants
+//
+//  Created by wgf on 16/9/21.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TRMainViewController : UIViewController
+
+@end
