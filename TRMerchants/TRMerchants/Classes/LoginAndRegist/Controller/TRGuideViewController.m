44f36377b95a6ec6b65bb64a4993c48999910b48
CoderGFWu <wgf@wgfdeMacBook-Air.local>
2016-09-26 17:13:59 +0800

    细节处理
    设置启动图, 完善登录界面

--- TRMerchants/TRMerchants/Classes/LoginAndRegist/Controller/TRGuideViewController.m
+++ TRMerchants/TRMerchants/Classes/LoginAndRegist/Controller/TRGuideViewController.m
@@ -10,6 +10,11 @@
 
 @interface TRGuideViewController ()
 
+/** 登录按钮 */
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+@property (weak, nonatomic) IBOutlet UIButton *registBtn;
+
 @end
 
 @implementation TRGuideViewController
@@ -17,6 +22,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.loginBtn.layer.borderWidth = 3;
+    self.loginBtn.layer.borderColor = [[UIColor redColor] CGColor];
+    self.loginBtn.layer.cornerRadius = 7;
+    
+    self.registBtn.layer.cornerRadius = 7;
+    
 }
 
 - (void)didReceiveMemoryWarning {
