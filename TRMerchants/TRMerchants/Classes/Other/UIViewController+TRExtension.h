1556eb1ec7bd1b0b5d3de32d0b7a6a5cca626fa3
CoderGFWu <wgf@wgfdeMacBook-Air.local>
2016-09-27 14:26:03 +0800

    大改动
    修改启动图片, 弃用xib, 改为storyboard, 配置pch文件, 创建UIViewController的分类

---
+++ TRMerchants/TRMerchants/Classes/Other/UIViewController+TRExtension.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+TRExtension.h
+//  TRMerchants
+//
+//  Created by wgf on 16/9/27.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (TRExtension)
+
+@end
---
+++ TRMerchants/TRMerchants/Classes/Other/UIViewController+TRExtension.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+TRExtension.m
+//  TRMerchants
+//
+//  Created by wgf on 16/9/27.
+//  Copyright © 2016年 wgf. All rights reserved.
+//
+
+#import "UIViewController+TRExtension.h"
+
+@implementation UIViewController (TRExtension)
+
+@end
